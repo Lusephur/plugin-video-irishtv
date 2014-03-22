e7247769e661fc256b7127d23793a5243b832a74
Mossy <mossy@wintermute.(none)>
2014-03-22 21:50:27 +0000

    Add resume support

---
+++ aertvplayer.py
@@ -0,0 +1,11 @@
+from player import BasePlayer
+
+class AerTVPlayer(BasePlayer):
+    def onPlayBackSeek(self, time, seekOffset):        
+        pass
+
+    def onPlayBackSeekChapter(self, chapter):
+        pass        
+
+    def onPlayBackSpeedChanged(self,speed):
+        pass
